4e5f8b95eb33b0b95e2f6d486c75815885ce1f97
Kyle Corbelli <pacificLiving@users.noreply.github.com>
2015-08-09 16:55:55 -0700

    Update quotes.js

--- public_html/js/quotes.js
+++ public_html/js/quotes.js
@@ -1,1 +1,21 @@
 var app = angular.module("quotesApp", []);
+
+// On page load:
+$(function() {
+    
+var $seagull = $("#seagull");
+setTimeout(function() {
+    $seagull.animate({height: "95px", width: "140px"}, 500, function() {
+        $seagull.animate({height: "54", width: "80"});
+    });
+}, 4000);
+
+$seagull.on("mousedown", function() {
+    $seagull.animate({height: "48", width: "72"}, 5);
+});
+
+$seagull.on("mouseup", function() {
+    $seagull.animate({height: "54", width: "80"}, 5);
+});
+
+});
