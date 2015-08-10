1ab64f05dc3713e8b96fef5a382ebd23f961f5f5
Kyle Corbelli <pacificLiving@users.noreply.github.com>
2015-08-09 17:47:34 -0700

    Update quotes.js

--- public_html/js/quotes.js
+++ public_html/js/quotes.js
@@ -5,17 +5,25 @@ $(function() {
     
 var $seagull = $("#seagull");
 setTimeout(function() {
-    $seagull.animate({height: "95px", width: "140px"}, 500, function() {
+    $seagull.animate({height: "108px", width: "160px"}, 500, function() {
         $seagull.animate({height: "54", width: "80"});
     });
 }, 4000);
 
+$seagull.on("mouseenter", function() {
+    $seagull.animate({height: "60", width: "88"}, 5);
+});
+
+$seagull.on("mouseleave", function() {
+    $seagull.animate({height: "54", width: "80"}, 5);
+});
+
 $seagull.on("mousedown", function() {
-    $seagull.animate({height: "48", width: "72"}, 5);
+    $seagull.animate({height: "54", width: "80"}, 5);
 });
 
 $seagull.on("mouseup", function() {
-    $seagull.animate({height: "54", width: "80"}, 5);
+    $seagull.animate({height: "60", width: "88"}, 5);
 });
 
 });
